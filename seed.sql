def7d335cae7c1a7e35b1ea1d028161f786ab364
cd <cd.skogsberg@gmail.com>
2021-09-27 17:21:33 +0200

    Fixed permissions for server.py. Added tables to SQL file for db:seed task.

--- seed.sql
+++ seed.sql
@@ -1,9 +1,30 @@
 CREATE TABLE
     IF NOT EXISTS
-        users(username
-            TEXT NOT NULL UNIQUE,
+        users (
+            username TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
             );
+CREATE TABLE
+    IF NOT EXISTS
+        posts (
+            id INTEGER NOT NULL UNIQUE,
+            username TEXT NOT NULL,
+            headline TEXT NOT NULL,
+            content TEXT NOT NULL
+            );
+CREATE TABLE
+    IF NOT EXISTS
+        sessions (
+            sid INTEGER NOT NULL UNIQUE,
+            username TEXT NOT NULL,
+            expires TEXT NOT NULL
+            );
 REPLACE INTO
-    users(username, password)
-    VALUES("login_test", "login");
+    users (
+        username,
+        password
+        )
+    VALUES (
+        "login_test",
+        "login"
+        );
