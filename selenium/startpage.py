700b9c3ba8375a9f7fd8c4005486e424dfe4e62f
cd <cd.skogsberg@gmail.com>
2021-09-28 16:45:27 +0200

    Starting to add webdriver?

--- selenium/startpage.py
+++ selenium/startpage.py
@@ -4,9 +4,12 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-print(type(driver))
-
 driver.get('http://localhost:8080/')
 
-elem = driver.find_element_by_id('login')
-assert 'Sign in' in elem
+sign_in = driver.find_element_by_id('login')
+sign_up = driver.find_element_by_id('signup')
+
+assert 'Sign in' in sign_in.text
+assert 'Sign up' in sign_up.text
+
+driver.quit()
