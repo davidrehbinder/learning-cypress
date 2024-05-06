3f637838d8e4523249beb6f2f1cb70ce4ac8df1b
davidrehbinder <david.rehbinder@gmail.com>
2024-05-06 14:33:06 +0200

    Moved config file.

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  video: false,
+
+  e2e: {
+    baseUrl: "http://localhost:8080",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    testIsolation: false,
+  },
+});
